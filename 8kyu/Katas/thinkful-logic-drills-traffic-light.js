b35dea746c61c4016cf4f6648d282e1c39e4ba0f
Ambar Avalos <ambaravalos1217@gmail.com>
2023-02-15 09:37:13 -0800

    added solution to 2nd kata

--- 8kyu/Katas/thinkful-logic-drills-traffic-light.js
+++ 8kyu/Katas/thinkful-logic-drills-traffic-light.js
@@ -9,6 +9,11 @@
 
 function updateLight(current) {
   
-    //your code here!
+        if (current == "green")
+          return "yellow" 
+        else if (current == "yellow")
+          return "red" 
+        else
+          return "green";
   
   }

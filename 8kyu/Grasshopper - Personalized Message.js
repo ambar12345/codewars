4c5a80ea1233bed68392369512694a1284df638b
Ambar Avalos <ambaravalos1217@gmail.com>
2023-03-08 09:02:47 -0800

    Added 3 8kyus

---
+++ 8kyu/Grasshopper - Personalized Message.js
@@ -0,0 +1,8 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+//My solution
+
+//arrow function with ternary operator checking if name equals owner
+const greet = (name,owner) => name == owner? 'Hello boss' : 'Hello guest'

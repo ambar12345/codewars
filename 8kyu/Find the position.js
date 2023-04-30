5d150de6ce2342e5c01cafd5962cafd8727db8b8
Ambar Avalos <ambaravalos1217@gmail.com>
2023-04-29 21:27:42 -0700

    added kata

--- 8kyu/Find the position.js
+++ 8kyu/Find the position.js
@@ -10,3 +10,6 @@ function position(letter){
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'
     return "Position of alphabet: " + (alphabet.indexOf(letter)+1)
         }
+
+//created string with letters of alphabet
+//returned a string and the index of the position of the letter plus 1 to indicate its place in the alphabet
